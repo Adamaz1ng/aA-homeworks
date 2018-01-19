b1bae74e2d7ada282a1175313532beea5516fd13
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-18 21:13:18 -0500

    Create map class - add methods to classes

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -1,4 +1,6 @@
 class Stack
+  attr_accessor :el_stack
+
   def initialize
     @el_stack = []
   end
@@ -17,6 +19,8 @@ class Stack
 end
 
 class Queue
+  attr_accessor :el_queue
+
   def initialize
     @el_queue = []
   end
@@ -31,5 +35,25 @@ class Queue
 
   def show
     @el_queue
+  end
+end
+
+class map
+  attr_accessor :map_hash
+
+  def initialize
+    @map_hash = Hash.new { |h, k| h[k] = nil }
+  end
+
+  def assign(key, value)
+    @map_hash[key] = value
+  end
+
+  def lookup(key)
+    @map_hash[key]
+  end
+
+  def remove(key)
+    @map.delete(key)
   end
 end
