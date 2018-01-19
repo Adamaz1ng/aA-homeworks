17deac2a9c36757c593899f00f4cfdf6513bb227
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-18 21:02:57 -0500

    Add queue class

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -1,17 +1,35 @@
 class Stack
-  def intialize
-
+  def initialize
+    @el_stack = []
   end
 
   def add(el)
-
+    @el_stack << el
   end
 
   def remove
-
+    @el_stack.pop
   end
 
   def show
+    @el_stack
+  end
+end
 
+class Queue
+  def initialize
+    @el_queue = []
+  end
+
+  def enqueue(el)
+    @el_queue.shift(el)
+  end
+
+  def dequeue
+    @el_queue.pop
+  end
+
+  def show
+    @el_queue
   end
 end
