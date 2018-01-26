e2f71814d3657eeb30ce273374720ad12aae6875
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-25 21:52:01 -0500

    Complete homework

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,17 +1,31 @@
 class LRUCache
-  def initialize
+  attr_accessor :cache_arr
+  attr_reader :max_size
+
+  def initialize(array_size)
+    @cache_arr = Array.new(array_size) { Array.new }
+    @max_size = array_size
   end
 
   def count
     # returns number of elements currently in cache
+    @cache_arr.reject { |el| el.empty? }.length
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    if self.count == @max_size
+      @cache_arr.shift
+      @cache_arr << [el]
+    else
+      @cache_arr[self.count] << el
+    end
+    @cache_arr
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    p @cache_arr
   end
 
   private
